d3146b980c55da2f7e90a057478eb71a8e4fb952
Tecelli Akıntuğ <tecelli.e.a@gmail.com>
2020-02-22 20:43:19 +0300

    First commit

---
+++ Assets/Scripts/Clicker/ClickerGameManager.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ClickerGameManager : MonoBehaviour
+{
+    public ClickerGameManager I { get; private set; }
+
+
+    [SerializeField] private TextMeshProUGUI cookieCountText;
+    [SerializeField] private TextMeshProUGUI cookieCpsText;
+    [SerializeField] private TextMeshProUGUI cookiesPerClickText;
+    [SerializeField] private TextMeshProUGUI cookieCpsCostText;
+    [SerializeField] private TextMeshProUGUI cookiesPerClickCostText;
+
+
+    private int _cookieCount;
+
+    public int CookieCount
+    {
+        get => _cookieCount;
+        set
+        {
+            _cookieCount = value;
+            cookieCountText.text = CookieCount + " Cookies";
+        }
+    }
+
+    private int _cookieCps;
+
+    public int CookieCps
+    {
+        get => _cookieCps;
+        set
+        {
+            _cookieCps = value;
+            cookieCpsText.text = CookieCps + "/s";
+        }
+    }
+
+    private int _cookiesPerClick;
+
+    public int CookiesPerClick
+    {
+        get => _cookiesPerClick;
+        set
+        {
+            _cookiesPerClick = value;
+            cookiesPerClickText.text = CookiesPerClick + " per Click";
+        }
+    }
+
+    private int _cookieCpsCost;
+
+    public int CookieCpsCost
+    {
+        get => _cookieCpsCost;
+        set
+        {
+            _cookieCpsCost = value;
+            cookieCpsCostText.text = "Cost: " + CookieCpsCost + " Cookies";
+        }
+    }
+
+    private int _cookiesPerClickCost;
+
+    public int CookiesPerClickCost
+    {
+        get => _cookiesPerClickCost;
+        set
+        {
+            _cookiesPerClickCost = value;
+            cookiesPerClickCostText.text = "Cost: " + CookiesPerClickCost + " Cookies";
+        }
+    }
+
+
+    private void Awake()
+    {
+        if (I == null)
+            I = this;
+        else
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        CookieCount = 0;
+        CookieCps = 0;
+        CookieCpsCost = 10;
+        CookiesPerClickCost = 5;
+        CookiesPerClick = 1;
+        InvokeRepeating(nameof(CookiesPerSecond), 1, 1);
+    }
+
+    public void ClickCookie()
+    {
+        CookieCount += CookiesPerClick;
+    }
+
+    public void CookiesPerSecond()
+    {
+        CookieCount += _cookieCps;
+    }
+
+    public void AddCps(int added)
+    {
+        if (CookieCount >= CookieCpsCost)
+        {
+            CookieCount -= CookieCpsCost;
+            CookieCpsCost *= 5;
+            CookieCps += added;
+        }
+    }
+
+    public void AddCpc(int added)
+    {
+        if (CookieCount >= CookiesPerClickCost)
+        {
+            CookieCount -= CookiesPerClickCost;
+            CookiesPerClickCost *= 2;
+            CookiesPerClick += added;
+        }
+    }
+}
---
+++ Assets/Scripts/Platformer/MovementManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour
+{
+    private int moveX;
+    private bool jumpInput;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    private Rigidbody2D _rigidbody2D;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        moveX = 0;
+
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpInput = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        var moveDir = new Vector2(moveX * moveSpeed, _rigidbody2D.velocity.y);
+        _rigidbody2D.velocity = (moveDir);
+        
+
+        if (jumpInput)
+        {
+            _rigidbody2D.AddForce(Vector2.up * jumpForce);
+            jumpInput = false;
+        }
+    }
+}
