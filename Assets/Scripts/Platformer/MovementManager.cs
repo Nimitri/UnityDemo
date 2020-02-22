042f88918e5270cde35f7bc6033adf7621331e59
Tecelli Akıntuğ <tecelli.e.a@gmail.com>
2020-02-22 23:09:20 +0300

    Implemented Ground Check

--- Assets/Scripts/Platformer/MovementManager.cs
+++ Assets/Scripts/Platformer/MovementManager.cs
@@ -11,6 +11,10 @@ public class MovementManager : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
 
+    [SerializeField] private Transform leftFoot;
+    [SerializeField] private Transform rightFoot;
+    [SerializeField] private LayerMask raycastMask;
+
     private Rigidbody2D _rigidbody2D;
 
     // Start is called before the first frame update
@@ -22,13 +26,25 @@ public class MovementManager : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        HandleInput();
+    }
+    
+    private void FixedUpdate()
+    {
+        HandleMovement();
+        HandleJump();
+    }
+
+    private void HandleInput()
+    {
         moveX = 0;
 
-        
+
         if (Input.GetKey(KeyCode.D))
         {
             moveX = 1;
         }
+
         if (Input.GetKey(KeyCode.A))
         {
             moveX = -1;
@@ -38,19 +54,32 @@ public class MovementManager : MonoBehaviour
         {
             jumpInput = true;
         }
-
     }
-
-    private void FixedUpdate()
+    
+    private void HandleMovement()
     {
         var moveDir = new Vector2(moveX * moveSpeed, _rigidbody2D.velocity.y);
         _rigidbody2D.velocity = (moveDir);
-        
+    }
 
-        if (jumpInput)
+    private void HandleJump()
+    {
+        if (jumpInput && CheckGrounding())
         {
             _rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            jumpInput = false;
         }
+        jumpInput = false;
+
+    }
+
+    private bool CheckGrounding()
+    {
+        Debug.DrawRay(leftFoot.position, Vector3.down * 0.5f, Color.red, 1f);
+        Debug.DrawRay(rightFoot.position, Vector3.down * 0.5f, Color.red, 1f);
+        
+        if(Physics2D.Raycast(rightFoot.position, Vector2.down, 0.5f, raycastMask))
+            if (Physics2D.Raycast(leftFoot.position, Vector2.down, 0.5f, raycastMask))
+                return true;
+        return false;
     }
 }
